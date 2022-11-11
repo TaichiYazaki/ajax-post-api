aca8358d9c79c0e1e5cb0e120ece4fb9d40ddb39
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-11-11 11:32:38 +0900

    不要なライブラリの削除

--- src/main/java/com/example/postapi/DTO/ZipCodeDTO.java
+++ src/main/java/com/example/postapi/DTO/ZipCodeDTO.java
@@ -1,6 +1,5 @@
 package com.example.postapi.DTO;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
--- src/main/java/com/example/postapi/PostSerivce.java
+++ src/main/java/com/example/postapi/PostSerivce.java
@@ -1,19 +1,15 @@
 package com.example.postapi;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.util.List;
 
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.postapi.DTO.Results;
 import com.example.postapi.DTO.ZipCodeDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
